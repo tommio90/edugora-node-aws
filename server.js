1abd79c296b3a8f1d58b8e731c1daed6b1732924
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-11-16 23:26:31 +0800

    fix server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello bitches')
 })
 
 app.listen(3007, function () {
-  console.log('Example app listening on port' + PORT)
+  console.log('Example app listening on port' );
 });
